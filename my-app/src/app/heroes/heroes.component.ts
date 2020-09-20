65d7a832a502a4b7d6f59667b7bc9a25fd2757eb
farjanahossain <farjana.96.hossain@gmail.com>
2020-09-20 19:08:27 +0600

    Add Services

--- my-app/src/app/heroes/heroes.component.ts
+++ my-app/src/app/heroes/heroes.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+//import {'Subscriber } from 'rxjs';
 import { Hero } from '../hero';
-import { HEROES } from './mock-heroes';
-
+import { HeroService } from '../hero.service';
+//import { HEROES } from './mock-heroes';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,7 +11,7 @@ import { HEROES } from './mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  heroes: Hero[];
 
   /*heroes = 'Windstorm';
   selectedHero: Hero;
@@ -19,18 +21,26 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm'*/
   
   selectedHero: Hero;
-  onSelect(hero: Hero):void {
-    this.selectedHero = hero;
+  //onSelect(hero: Hero):void {
+   // this.selectedHero = hero;
+  //}
+  
+  constructor(private heroService: HeroService , private messageService: MessageService) { }
+  ngOnInit() {
+    this.getHeroes();
   }
   
-  constructor() { }
-  ngOnInit() {
-  }
 
   /*ngOnInit(): void {
   }*/
-  /*onSelect(hero: Hero): void {
+  onSelect(hero: Hero): void {
     this.selectedHero = hero;
-  }*/
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+
+  }
+  getHeroes():void{
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes );
+  }
 
 }
---
+++ my-app/src/app/messages/messages.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+//import { Hero } from '../hero';
+//import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service'
+//import { from } from 'rxjs';
+@Component({
+  selector: 'app-messages',
+  templateUrl: './messages.component.html',
+  styleUrls: ['./messages.component.css']
+})
+export class MessagesComponent implements OnInit {
+
+
+  constructor(public messageService: MessageService) { }
+
+  ngOnInit(): void {
+  }
+
+}
